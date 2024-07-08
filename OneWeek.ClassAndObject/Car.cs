514166788f94ff05bcd3e92cc3999ab88d4c6759
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2024-07-08 17:15:37 +0500

    ...

---
+++ OneWeek.ClassAndObject/Car.cs
@@ -0,0 +1,13 @@
+namespace OneWeek.ClassAndObject;
+
+public class Car
+{
+    public string Model { get; set; }
+    public string Color { get; set; }
+    public int Year { get; set; }
+
+    public void Drive()
+    {
+        Console.WriteLine("The car is driving");
+    }
+}
--- OneWeek.ClassAndObject/Program.cs
+++ OneWeek.ClassAndObject/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using OneWeek.ClassAndObject;
+
+Car newCar = new Car();
+newCar.Model = "BMW";
+newCar.Color = "Black";
+newCar.Year = 2024;
+
+newCar.Drive();
