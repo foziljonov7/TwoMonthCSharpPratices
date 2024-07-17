328a589477319827590b01d7d9b9184a67c86a75
MrUstoz <Tahsimuz@gmail.com>
2024-07-17 14:32:37 +0500

    Task and Parallel programming

--- FifeWeek.Asynchrouns/Program.cs
+++ FifeWeek.Asynchrouns/Program.cs
@@ -1,1 +1,55 @@
-﻿
+﻿//Async & await
+// async Task<int> GetNumberWithExceptionHandlingAsync()
+// {
+//     try
+//     {
+//         await Task.Delay(1000);
+//         return 100;
+//     }
+//     catch (Exception ex)
+//     {
+//         Console.WriteLine($"Error: {ex.Message}");
+//         return -1;
+//     }
+// }
+
+// int res = await GetNumberWithExceptionHandlingAsync();
+// Console.WriteLine(res);
+
+//Task - parallel
+
+// async void DoSomeWork()
+// {
+//     Task.Delay(1000).Wait();
+//     Console.WriteLine("Do some work");
+// }
+// async void DoSomeOtherWork()
+// {
+//     Task.Delay(500).Wait();
+//     Console.WriteLine("Do some other work");
+// }
+
+// Parallel.Invoke(() => DoSomeWork(), () => DoSomeOtherWork());
+
+// Task t1 = Task.Run(() => PerformTask(1));
+// Task t2 = Task.Run(() => PerformTask(2));
+// Task t3 = Task.Run(() => PerformTask(3));
+
+// await Task.WhenAll(t1, t2, t3);
+// Console.WriteLine("All tasks completed");
+
+// static void PerformTask(int id)
+// {
+//     Console.WriteLine($"Task {id} is running");
+//     Task.Delay(1000).Wait();
+//     Console.WriteLine($"Task {id} is completed");
+// }
+
+//Task library
+// Parallel.For(0, 10, i =>
+// {
+//     Console.WriteLine($"Element {i} ni qayta ishlash {Task.CurrentId}");
+//     Task.Delay(100).Wait();
+// });
+
+// Console.WriteLine("Barchasi qayta ishlandi.");
--- FifeWeek.Asynchrouns/obj/Debug/net8.0/FifeWeek.Asynchrouns.AssemblyInfo.cs
+++ FifeWeek.Asynchrouns/obj/Debug/net8.0/FifeWeek.Asynchrouns.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FifeWeek.Asynchrouns")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05ee521cbc992eaf2ac77a56d24e5e2dbc0cd97b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5b8025eea170f1633dc4e846174d191e76ce94c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FifeWeek.Asynchrouns")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FifeWeek.Asynchrouns")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
